f036201dcfdd753bcb79c86a0a595873e23b8b29
unknown <danenkov.dmitriy@list.ru>
2022-08-14 18:47:09 +0300

    h9

---
+++ HomeWork9/Employee.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork9
+{
+    internal class Employee
+    {
+        private int _age;
+        private int _exp;
+        //public Employee(string name, int age, int exp, bool checkOfEducation)
+        //{
+        //    Age = age;
+        //    Exp = exp;
+        //    Name = name;
+        //    CheckOfEducation = checkOfEducation;
+        //}
+
+        public string Name { get; set; }
+        public int  Age 
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _age = 0;
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+        public int  Exp
+        { 
+            get
+            {
+                return _exp;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _exp = 0;
+                }
+                else
+                {
+                    _exp = value;
+                }
+            }
+        }
+        public bool  CheckOfEducation { get; set; }
+        public static int GetSalary(Employee employee)
+        { 
+           if (employee.CheckOfEducation)
+            {
+                return (employee.Exp + 1) * 1250;
+            }
+           else
+            {
+                return (employee.Exp + 1) * 1000;
+            }
+        }
+
+        public static void PrintSalary(List<Employee> employeers)
+        {
+            foreach (Employee employee in employeers)
+            {
+                if (employee.CheckOfEducation)
+                {
+                    Console.WriteLine($"ЗП С ВЫШКОЙ:{employee.Name} - {(employee.Exp + 1) * 1250}");
+                }
+                else
+                {
+                    Console.WriteLine($"ЗП Без вышки:{employee.Name} - {(employee.Exp + 1) * 1000}");
+                }
+            }
+        }
+
+
+
+
+
+
+
+        public static void PrintListOfSalary(List<Employee> employers, Program.GetSalaryMethod delegateSalary)
+        {
+            foreach (var employee in employers)
+            {
+                int salary = delegateSalary(employee);
+                Console.WriteLine($"{employee.Name} - {salary}");
+            }
+
+        }
+
+        public static void PrintSalary(List<Employee> employees, Program.GetSalaryMethod delegateSalary)
+        {
+            foreach (var employee in employees)
+            {
+                int salary = delegateSalary(employee);
+                Console.WriteLine($"{employee.Name} - {salary}");
+            }
+        }
+
+    }
+}
---
+++ HomeWork9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork9
+{
+    internal class Program
+    {
+        public delegate int GetSalaryMethod(Employee employee);
+        public static int GetSalary(Employee employee)
+        {
+            return (employee.Exp + 1) * 1000;
+        }
+        static void Main(string[] args)
+        {
+            
+            //Employee emp = new Employee("d", 2, 2, true);
+            List<Employee> employeeList = new List<Employee>
+            {
+                new Employee() { Name = "Dasha", Age = 18, Exp = 2, CheckOfEducation = false },
+                new Employee() { Name = "Dima", Age = 23, Exp = 3, CheckOfEducation = true },
+                new Employee() { Name = "Volodya", Age = 32, Exp = 7, CheckOfEducation = false },
+                new Employee() { Name = "Nikita", Age = 22, Exp = 4, CheckOfEducation = true }
+            };
+            Console.WriteLine("Просто статический метод ПринтСэлэри:");
+            
+            Employee.PrintSalary(employeeList);
+
+            Console.WriteLine("------------------------------------------------------");
+            
+            Console.WriteLine("Статический метод,с делегатом:");
+            //GetSalaryMethod salaryDelegate = new GetSalaryMethod(Employee.GetSalary); 2й способ
+            GetSalaryMethod salaryDeleagate = GetSalary;
+            Employee.PrintSalary(employeeList, salaryDeleagate);
+            Console.WriteLine("------------------Test-------------------");
+            Employee.PrintListOfSalary(employeeList, salaryDeleagate);
+
+            Console.WriteLine("------------------------------------------------------");
+
+            Console.WriteLine("Метод без реализации лямбды:");
+            salaryDeleagate = GetSalary;
+            Employee.PrintSalary(employeeList, salaryDeleagate);
+
+            Console.WriteLine("------------------------------------------------------");
+
+            Console.WriteLine("Использование лямбды уже со своим условием (стрелочки рулят)");
+            Employee.PrintSalary(employeeList, (employee) => (employee.Exp + 1) * 3000);
+           
+
+
+            
+        }
+    }
+}
